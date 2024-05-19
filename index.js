90f5422e73bc5b25db15ca8ee472eb28adc4317e
Fahad <fahadabdulqayyum@focuspass.com>
2024-05-19 14:55:32 +0500

    first commit as usual

---
+++ index.js
@@ -0,0 +1,5 @@
+function sum(a, b) {
+    return a + b;
+}
+var add = sum(4, 6);
+console.log("add", add);
---
+++ index.ts
@@ -0,0 +1,5 @@
+function sum(a: number, b: number): number {
+  return a + b;
+}
+const add = sum(4, 6);
+console.log("add", add);
